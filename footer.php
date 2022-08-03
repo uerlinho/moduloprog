ff1cb63ed242370bcf7c3da798f08990ce4eaa51
Wesley Thyago <wesley.thyago16@gmail.com>
2022-08-03 15:19:39 -0300

    Footer ajustado com o crédito e o link de suporte

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     echo "
     <footer>
         <div id='parte-1'>...</div>
-        <div id='parte-2'>...</div>
+    <div id='parte-2'>Developed by Wesley Thyago • <a href='mailto:wesley.thyago16@gmail.com?subject=Suporte GPM - [Digite a sua dúvida aqui]'>Suporte</a></div>
     </footer>    
     ";
 ?>
--- menu.php
+++ menu.php
@@ -5,23 +5,23 @@
         <div class='sub-sub-menu'>
             <li onclick='abrirSubMenu1()'>Bases</li>
             <div id='sub-sub-sub-menu-1'>
-                <li><a href=''>Relatório Gerencial</a></li>
-                <li><a href=''>Tabela B2</a></li>
-                <li><a href=''>Input Bases</a></li>
+                <li><a href='Demandas_Invest.php'>Relatório Gerencial</a></li>
+                <li><a href='Demandas_Despesa.php'>Tabela B2</a></li>
+                <li><a href='Input_bases.php'>Input Bases</a></li>
             </div>
             <li onclick='abrirSubMenu2()'>Planejamento</li>
             <div id='sub-sub-sub-menu-2'>
-                <li><a href=''>Programações</a></li>
-                <li><a href=''>Calendário</a></li>
-                <li><a href=''>Quadro de liberação</a></li>
-                <li><a href=''>Desligamentos</a></li>
-                <li><a href=''>Requisição de Material</a></li>
+                <li><a href='Programacoes.php'>Programações</a></li>
+                <li><a href='Programacoes_calendario.php'>Calendário</a></li>
+                <li><a href='Quadro_liberacao.php'>Quadro de liberação</a></li>
+                <li><a href='Desligamentos.php'>Desligamentos</a></li>
+                <li><a href='Requisicoes_material.php'>Requisição de Material</a></li>
             </div>
             <li onclick='abrirSubMenu3()'>Encerramento</li>
             <div id='sub-sub-sub-menu-3'>
-                <li><a href=''>Demandas</a></li>
-                <li><a href=''>DExPARA</a></li>
-                <li><a href=''>Solicitações</a></li>
+                <li><a href='Encerramento_obras.php'>Demandas</a></li>
+                <li><a href='dexpara.php'>DExPARA</a></li>
+                <li><a href='Solicitacoes.php'>Solicitações</a></li>
             </div>
             <li onclick='abrirSubMenu4()'>Relatórios</li>
             <div id='sub-sub-sub-menu-4'>
